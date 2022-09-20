fc355b09b718deae67e36ce494692261169db86f
Caleb <calebwagner72@yahoo.com>
2022-09-20 16:15:04 -0500

    clean coding - extracting methods

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String message = greetUser("Mosh", "Hami");
-
-
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         int principle = 0;
-        float monthlyInterest = 0;
-        int numberOfPayments = 0;
+        byte years = 0;
+        float annualInterest = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -29,38 +23,50 @@ public class Main {
 
         while (true) {
             System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            if (principle >= 1 && principle <= 30) {
-                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            annualInterest = scanner.nextFloat();
+            if (principle >= 1 && principle <= 30)
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
         while (true) {
             System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            if (years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30)
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
-        double mortgage = principle
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) -1));
+        double mortgage = calculateMortgage(principle, annualInterest, years);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.print("Mortgage: " + mortgageFormatted);
 
     }
 
+    public static double calculateMortgage(
+            int principle,
+            float annualInterest,
+            byte years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+
+        double mortgage = principle
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) -1));
+
+        return mortgage;
+    }
+
 //    public static void greetUser(String firstName, String lastName) {
 //        System.out.println("Hello " + firstName + " " + lastName);
 //    }
 
-    public static String greetUser(String firstName, String lastName) {
-    return "Hello " + firstName + " " + lastName;
-}
+//    public static String greetUser(String firstName, String lastName) {
+//    return "Hello " + firstName + " " + lastName;
+//}
 }
